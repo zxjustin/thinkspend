0e971051be9a558351ceea3fd5443c51b7a5be4d
zxjustin <zxjustin@hotmail.com>
2025-10-29 22:26:34 +0800

    Update folder items

--- src/components/notes/FolderTree.vue
+++ src/components/notes/FolderTree.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="folder-tree">
-    <FolderItem 
-      v-for="folder in folders" 
+    <FolderItem
+      v-for="folder in folders"
       :key="folder.id"
       :folder="folder"
       :depth="depth"
+      :expand-signal="expandSignal"
+      :collapse-signal="collapseSignal"
     />
   </div>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
 import FolderItem from './FolderItem.vue'
 
-defineProps({
+const props = defineProps({
   folders: Array,
   depth: {
     type: Number,
     default: 0
-  }
+  },
+  expandSignal: Number,
+  collapseSignal: Number
 })
 </script>
